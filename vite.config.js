80afaa5121b2cdebb1d6ad81acf578e9071e7e8a
mojtabaAPK <mojtaba.apk@gmail.com>
2026-07-27 20:15:52 +0330

    set github action

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import tailwindcss from "@tailwindcss/vite";
 import { fileURLToPath, URL } from 'node:url';
 
 export default defineConfig({
-	//base: "/vuejs/",
+	base: "/vuejs/",
 	resolve: {
 		alias: {
 			'@': fileURLToPath(new URL('./src', import.meta.url)),
